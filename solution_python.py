9de483c698313666b3209f4e3f88e0428d7d80c4
Simbobo <53153208+Simbobo@users.noreply.github.com>
2020-07-10 14:23:57 -0400

    Add files via upload

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,57 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.eventList = [0]
+        self.index = 1
+        self.redoAmount = 0
 
     def add(self, num: int):
+        self.value += num
+        self.eventList = self.eventList[:self.index]
+        self.eventList.append(self.value)
+        self.index += 1
+        self.redoAmount = 0
         pass
 
     def subtract(self, num: int):
+        self.value -= num
+        self.eventList = self.eventList[:self.index]
+        self.eventList.append(self.value)
+        self.index += 1
         pass
 
     def undo(self):
+        if self.index > 1:
+            self.index -= 1
+            self.value = self.eventList[self.index-1]
+            self.redoAmount += 1
         pass
 
     def redo(self):
+        if self.redoAmount > 0:
+            self.index += 1
+            self.value = self.eventList[self.index-1]
+            self.redoAmount -= 1
         pass
 
     def bulk_undo(self, steps: int):
+        if self.index > steps:
+            self.index -= steps
+            self.value = self.eventList[self.index-1]
+            self.redoAmount += steps
+        else:
+            self.redoAmount = self.index-1
+            self.index = 1
+            self.value = self.eventList[self.index-1]
         pass
 
     def bulk_redo(self, steps: int):
+        if self.redoAmount > steps-1:
+            self.index += steps
+            self.value = self.eventList[self.index-1]
+            self.redoAmount -= steps
+        else:
+            self.index += self.redoAmount
+            self.value = self.eventList[self.index-1]
+            self.redoAmount = 0
         pass
